9fe2f0afb2f2d397a68af3a1a08389f44c4512ad
zhangting <89507288@qq.com>
2020-07-14 12:22:18 +0800

    CICD-0000 fix

--- 22.060/_test_happy_birthday.py
+++ 22.060/_test_happy_birthday.py
@@ -1,4 +1,4 @@
-# import requests
-# r=requests.get("http://localhost:8000/happy_birthday?name=ting&age=18")
-# print(r.status_code)
-# print(r.text)
+import requests
+r=requests.get("http://localhost:8000/happy_birthday?name=ting&age=18")
+print(r.status_code)
+print(r.text)
