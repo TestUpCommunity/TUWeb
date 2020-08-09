d4d01c48ddcbdc8b7e8be0690776f149a706e57b
zhangting <89507288@qq.com>
2020-08-09 10:26:19 +0800

    test

--- 22.140/_test_auth_2_with_session.py
+++ 22.140/_test_auth_2_with_session.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 import requests
 url = 'http://localhost:8000/token_generation'
 payload = {
@@ -11,6 +12,11 @@ print(r.text)
 s=requests.Session()
 s.headers["Authorization"]=r.json()["token"]
 url = 'http://localhost:8000/token_authenticated'
-r = s.get(url)
-print(r)
-print(r.text)
+r1 = s.get(url)
+print(r1)
+print(r1.text)
+
+#另一种写法，这里是为了回复一个读者提问增加的
+r2=requests.get(url,headers={"Authorization":r.json()["token"]})
+print(r2)
+print(r2.text)
