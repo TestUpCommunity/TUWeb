f9b04969e4445f1c9472956f53b9b57490307ea0
zhangting <89507288@qq.com>
2020-10-17 08:45:44 +0800

    test

---
+++ 22.220/_test_auth_baidu.py
@@ -0,0 +1,36 @@
+# -*- coding: gbk -*-
+import requests
+
+#case 1
+url = 'http://localhost:8000/oauth/2.0/token'
+r = requests.get(url)
+print(r)
+print(r.text)
+
+#case 2
+url = 'http://localhost:8000/oauth/2.0/token'
+r = requests.get(url,data={"client_id":"Va5yQRHlA4Fq5eR3LT0vuXV4","client_secret":"0rDSjzQ20XUj5itV6WRtznPQSzr5pVw2",
+                           "grant_type":"client_credentials"})
+print(r)
+print(r.text)
+
+#case 3
+url = 'http://localhost:8000/oauth/2.0/token'
+r = requests.post(url,data={"client_id":"Va5yQRHlA4Fq5eR3LT0vuXV4","client_secret":"0rDSjzQ20XUj5itV6WRtznPQSzr5pVw2",
+                           "grant_type":"client_credentials"})
+print(r)
+print(r.text)
+
+#Á·Ï° 1
+url = 'http://localhost:8000/oauth/2.0/token'
+r = requests.post(url,data={"client_id":"xVa5yQRHlA4Fq5eR3LT0vuXV4","client_secret":"0rDSjzQ20XUj5itV6WRtznPQSzr5pVw2",
+                           "grant_type":"client_credentials"})
+print(r)
+print(r.text)
+
+##Á·Ï° 2
+url = 'http://localhost:8000/oauth/2.0/token'
+r = requests.post(url,data={"client_id":"Va5yQRHlA4Fq5eR3LT0vuXV4","client_secret":"X0rDSjzQ20XUj5itV6WRtznPQSzr5pVw2",
+                           "grant_type":"client_credentials"})
+print(r)
+print(r.text)
