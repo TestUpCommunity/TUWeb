dedc603a40b2b0982a716d2c24ede7f133abb874
zhangting <89507288@qq.com>
2020-10-17 10:31:09 +0800

    test

--- 22.220/_test_auth_baidu.py
+++ 22.220/_test_auth_baidu.py
@@ -1,36 +1,42 @@
 # -*- coding: gbk -*-
 import requests
 
-#case 1
-url = 'http://localhost:8000/oauth/2.0/token'
-r = requests.get(url)
-print(r)
-print(r.text)
-
-#case 2
-url = 'http://localhost:8000/oauth/2.0/token'
-r = requests.get(url,data={"client_id":"Va5yQRHlA4Fq5eR3LT0vuXV4","client_secret":"0rDSjzQ20XUj5itV6WRtznPQSzr5pVw2",
-                           "grant_type":"client_credentials"})
-print(r)
-print(r.text)
-
-#case 3
+#登陆获取token
 url = 'http://localhost:8000/oauth/2.0/token'
 r = requests.post(url,data={"client_id":"Va5yQRHlA4Fq5eR3LT0vuXV4","client_secret":"0rDSjzQ20XUj5itV6WRtznPQSzr5pVw2",
                            "grant_type":"client_credentials"})
 print(r)
 print(r.text)
 
-#练习 1
-url = 'http://localhost:8000/oauth/2.0/token'
-r = requests.post(url,data={"client_id":"xVa5yQRHlA4Fq5eR3LT0vuXV4","client_secret":"0rDSjzQ20XUj5itV6WRtznPQSzr5pVw2",
-                           "grant_type":"client_credentials"})
+#调用接口
+url = 'http://localhost:8000/text2audio'
+token = r.json()['access_token']
+tex = "三分钟前，由北京市顺义区二经路与二纬路交汇处北侧，北京首都国际机场T3航站楼 去往 东城区北三环东路36号喜来登大酒店(北京金隅店)"
+
+#用错误的token调用接口
+payload =  {'tok': "fail", 'tex': tex, 'cuid': "quickstart",
+              'lan': 'zh', 'ctp': 1}
+r = requests.post(url,data=payload)
 print(r)
 print(r.text)
 
-##练习 2
-url = 'http://localhost:8000/oauth/2.0/token'
-r = requests.post(url,data={"client_id":"Va5yQRHlA4Fq5eR3LT0vuXV4","client_secret":"X0rDSjzQ20XUj5itV6WRtznPQSzr5pVw2",
-                           "grant_type":"client_credentials"})
+#用正确token调用接口
+payload =  {'tok': token, 'tex': tex, 'cuid': "quickstart",
+              'lan': 'zh', 'ctp': 1}
+r = requests.post(url,data=payload)
+print(r)
+print(r.text)
+
+#用错误的参数调用接口，此处遗漏一个必填参数ctp
+payload =  {'tok': token, 'tex': tex, 'cuid': "quickstart",
+              'lan': 'zh'}
+r = requests.post(url,data=payload)
+print(r)
+print(r.text)
+
+#用错误的参数调用接口，此处ctp传了不允许的值2
+payload =  {'tok': token, 'tex': tex, 'cuid': "quickstart",
+              'lan': 'zh', 'ctp': 2}
+r = requests.post(url,data=payload)
 print(r)
 print(r.text)
--- 22.220/auth_baidu.py
+++ 22.220/auth_baidu.py
@@ -1,4 +1,5 @@
-import hug
+# -*- coding: gbk -*-
+import hug,falcon
 
 @hug.post("/oauth/2.0/token", output=hug.output_format.json)
 @hug.get("/oauth/2.0/token", output=hug.output_format.json)
@@ -12,6 +13,7 @@ def get_oauth_token(client_id,client_secret,grant_type):
   "expires_in": 2592000,
   "scope": "public wise_adapt",
   "session_key": "9mzdDZXu3dENdFZQurfg0Vz8slgSgvvOAUebNFzyzcpQ5EnbxbF+hfG9DQkpUVQdh4p6HbQcAiz5RmuBAja1JJGgIdJI",
+   # 本来这里的access token应该是通过算法加secret key计算出来的
   "access_token": "24.6c5e1ff107f0e8bcef8c46d3424a0e78.2592000.1485516651.282335-8574074",
   "session_secret": "dfac94a3489fe9fca7c3221cbf7525ff"
 }
@@ -30,6 +32,32 @@ def get_oauth_token(client_id,client_secret,grant_type):
         }
     else:
         return None # grant_type not match
+
+
+
+
+@hug.post("/text2audio")
+@hug.get("/text2audio")
+def text2audio(body,response):
+
+    access_token = body.get('tok')#用get防止客户端没传tok的时候服务端出现语法错误
+    #下面这个写死的token应该是从数据库里读出来的，此处写死只是为了简便示意。
+    if access_token != "24.6c5e1ff107f0e8bcef8c46d3424a0e78.2592000.1485516651.282335-8574074":
+        response.status = falcon.HTTP_502 #错误代码是用falcon自带的
+        return {"err_no":502, "err_msg":"auth failed.","sn":"abcdefgh","idx":1} #错误信息按照官方文档的500错误信息编的
+    #必填项有没有填的校验
+    if (body.get('tex') and body.get('cuid') and body.get('ctp') and body.get('lan')) == None:
+        response.status = falcon.HTTP_501
+        return {"err_no":501, "err_msg":"input param incorrect.","sn":"abcdefgh","idx":2}
+    #必填项的校验
+    if len(body['tex'])>2048 or len(body['cuid'])>60 or len(body['ctp'])!=1 or len(body['lan'])!='zh':
+        response.status = falcon.HTTP_501
+        return {"err_no": 501, "err_msg": "input param incorrect.", "sn": "abcdefgh", "idx": 2}
+    #选填项的校验,我写了一个作为例子，剩下的请读者自己做练习。
+    if body.get('spd') and body.get('spd') not in range(0,16):
+        response.status = falcon.HTTP_501
+        return {"err_no": 501, "err_msg": "input param incorrect.", "sn": "abcdefgh", "idx": 2}
+
 
 if __name__ == "__main__":
     hug.API(__name__).http.serve(port=8000)
