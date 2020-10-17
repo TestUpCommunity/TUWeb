d14539c015d5b397957ffb4b9dea5441b8bfd353
zhangting <89507288@qq.com>
2020-10-17 08:34:47 +0800

    test

--- 22.210/_test_auth_baidu.py
+++ 22.210/_test_auth_baidu.py
@@ -1,3 +1,4 @@
+# -*- coding: gbk -*-
 import requests
 
 #case 1
@@ -16,6 +17,20 @@ print(r.text)
 #case 3
 url = 'http://localhost:8000/oauth/2.0/token'
 r = requests.post(url,data={"client_id":"Va5yQRHlA4Fq5eR3LT0vuXV4","client_secret":"0rDSjzQ20XUj5itV6WRtznPQSzr5pVw2",
+                           "grant_type":"client_credentials"})
+print(r)
+print(r.text)
+
+#Á·Ï° 1
+url = 'http://localhost:8000/oauth/2.0/token'
+r = requests.post(url,data={"client_id":"xVa5yQRHlA4Fq5eR3LT0vuXV4","client_secret":"0rDSjzQ20XUj5itV6WRtznPQSzr5pVw2",
+                           "grant_type":"client_credentials"})
+print(r)
+print(r.text)
+
+##Á·Ï° 2
+url = 'http://localhost:8000/oauth/2.0/token'
+r = requests.post(url,data={"client_id":"Va5yQRHlA4Fq5eR3LT0vuXV4","client_secret":"X0rDSjzQ20XUj5itV6WRtznPQSzr5pVw2",
                            "grant_type":"client_credentials"})
 print(r)
 print(r.text)
